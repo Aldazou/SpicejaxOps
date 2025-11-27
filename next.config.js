288be06547af5d796bda1557941f0d92f9b5529b
Your Name <your.email@example.com>
2025-11-27 11:49:41 -0500

    Add Google image domains to Next.js config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        pathname: '/**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
-
